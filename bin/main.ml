0ac480e4b49896995169baf0604a1dfff6708bc8
Will Banks <williamjb555@gmail.com>
2024-10-11 17:50:27 +0100

    Wrote rest of eval_tok function

--- bin/main.ml
+++ bin/main.ml
@@ -9,4 +9,5 @@ let get_lines () = let lines_mut = ref [] in
   with End_of_file -> !lines_mut ;;
 
 let () = let toks = get_lines () |> List.map (Brainfuck.parse_bytes) |> List.concat in
-  List.length toks |> printf "Num tokens: %i\n" ;;
+  List.length toks |> printf "Num tokens: %i\n"; ;;
+   (* TODO: Write interpreter *)
--- lib/brainfuck.ml
+++ lib/brainfuck.ml
@@ -2,7 +2,7 @@ type bf_tok = None | Forwards | Backwards | Increment | Decrement | Output | Inp
 
 exception InvalidCommand
 
-exception Todo
+exception NoMatchingBracket
 
 let to_tok b = match b with
   | '>' -> Forwards
@@ -18,25 +18,38 @@ let to_tok b = match b with
 
 let parse_bytes b = b |> String.to_seq |> List.of_seq |> List.map to_tok |> List.filter (fun t -> t != None)
 
-let eval_tok tok tape ptr in_chan out_chan = match tok with
+let eval_tok toks tape ptr in_chan out_chan = match ptr |> List.nth toks with
   | None -> (tape, ptr)
-  | Forwards -> (tape, ptr + 1)
+  | Forwards -> (
+    (if (tape |> List.length) <= (ptr + 1) then tape @ [0] else tape),
+    ptr + 1
+  )
   | Backwards -> (tape, Int.max 0 (ptr - 1))
   | Increment -> (
     tape |> List.mapi (fun i n -> if i = ptr then n + 1 else n),
-    ptr
+    ptr + 1
   )
   | Decrement -> (
     tape |> List.mapi (fun i n -> if i = ptr then n + 1 else n),
-    ptr
+    ptr + 1
   )
   | Output -> (
     tape |> List.mapi (fun i n -> if i = ptr then (n |> Char.chr |> output_char out_chan; n) else n),
-    ptr
+    ptr + 1
   )
   | Input -> (
     tape |> List.mapi (fun i n -> if i = ptr then (input_char in_chan) |> Char.code else n),
-    ptr
+    ptr + 1
   )
-  (* TODO: Finish *)
-  | _ -> raise Todo
+  | JumpForwardsIfZero -> (
+    tape,
+    match toks |> List.find_mapi (fun i t -> if i > ptr && t = JumpBackwardsIfnZero then Some (i + 1) else None) with
+      | Some i -> i
+      | None -> raise NoMatchingBracket
+  )
+  | JumpBackwardsIfnZero -> (
+    tape,
+    match toks |> List.mapi (fun i t -> (i, t)) |> List.rev |> List.find_map (fun (i, t) -> if i > ptr && t = JumpBackwardsIfnZero then Some (i + 1) else None) with
+      | Some i -> i
+      | None -> raise NoMatchingBracket
+  )
